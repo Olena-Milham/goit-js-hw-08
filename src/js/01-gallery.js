7f193983d0d33c3adca65bdafb9bc472ff9b1b72
Olena-Milham <nikimilh@gmail.com>
2022-05-13 13:57:16 +0300

    add template

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,3 +1,5 @@
+// import galleryItemTml from '../templates/gallery-item.hbs';
+import galleryItemsTml from '../templates/gallery-items.hbs';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
@@ -6,22 +8,15 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 const galleryEl = document.querySelector('.gallery');
-const markup = galleryItems
-  .map(
-    item => `<li class="gallery__item">
-  <a class="gallery__link" href="${item.original}">
-    <img
-      class="gallery__image"
-      src="${item.preview}"
-      data-source="${item.original}"
-      alt="${item.description}"
-    />
-  </a>
-</li>`,
-  )
-  .join('');
+// const markup = galleryItems.map(galleryItemTml).join('');
+
+const markup = createItemsMarkup(galleryItems);
 
 galleryEl.insertAdjacentHTML('afterbegin', markup);
+
+function createItemsMarkup(galleryItems) {
+  return galleryItemsTml(galleryItems);
+}
 
 new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
@@ -29,3 +24,14 @@ new SimpleLightbox('.gallery a', {
 });
 
 console.log(galleryItems);
+
+//     item => `<li class="gallery__item">
+//   <a class="gallery__link" href="${item.original}">
+//     <img
+//       class="gallery__image"
+//       src="${item.preview}"
+//       data-source="${item.original}"
+//       alt="${item.description}"
+//     />
+//   </a>
+// </li>`,
